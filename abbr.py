c463e6725a6982dd05f3662c172b7c702e1277d9
Jack Weinbender <jack.weinbender@gmail.com>
2017-07-10 16:49:44 -0500

    Added  abbr.py filter

--- abbr.py
+++ abbr.py
@@ -4,21 +4,29 @@
 
 """
 
-from pandocfilters import toJSONFilter, Str
+from pandocfilters import toJSONFilter, Str, Emph
 import re
 
 abbreviations = {
-    "jub"   : "Jubilees",
-    "ga"    : "Genesis Apocryphon",
-    "ant"   : "Jewish Antiquities",
-    "lab"   : "Liber antiquitatum biblicarum"
+    "jub"     : "Jubilees",
+    "ga"      : "Genesis Apocryphon",
+    "ant"     : "Jewish Antiquities",
+    "lab"     : "Liber antiquitatum biblicarum"
+}
+abbr_emph = {
+    "stj-full": "Scripture and Tradition in Judaism: Haggadic Studies",
+    "stj"     : "Scripture and Tradition" 
 }
 
-def expand(key, value, format, meta):
-  if key == 'Str' and ('$' in value):
-    m = re.compile('.*\$(\w+)')
+def expand(key, value, _format, _meta):
+    if key == 'Str' and ('$' in value):
+        r = re.compile('(.*)\$([\w|-]+)(.*)')
+        m = r.match(value)
 
-    return Str(m.match(value).group(1) + "MATCH")
+        if abbr_emph.has_key(m.group(2)):
+            return [Str(m.group(1)), Emph([Str(abbr_emph[m.group(2)])]), Str(m.group(3))]
+        else:
+            return [Str(m.group(1)), Str(abbreviations[m.group(2)]), Str(m.group(3))]
 
 if __name__ == "__main__":
-  toJSONFilter(expand)
+    toJSONFilter(expand)
